00c990953b9bba5fe0b19a33893b93b12f69fe86
Joshua Chasnov <jjc914@student.cis.edu.hk>
2022-03-22 14:15:31 +0800

    Initial Commit

---
+++ RayTraceCPU/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  RayTraceCPU
+//
+//  Created by Joshua Chasnov on 22/3/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
